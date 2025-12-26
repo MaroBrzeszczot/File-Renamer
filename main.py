ab4b07df37b2b11386acccdd6a663afae3a50271
ja <52638961+MatimoleQQ@users.noreply.github.com>
2025-12-26 17:09:41 +0100

    grupuje w podfolderach z nazwami rozszerzen

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ nazwa = input("Podaj nazwę bazową: ")
 
 pliki = os.listdir(folder_path)
 licznik = 1
-
+grupujPoExt = True
 for plik in pliki:
     file_path = os.path.join(folder_path, plik)
 
 
     if os.path.isfile(file_path):
         # print("Plik: ",plik)
-        rozszerzenie = os.path.splitext(plik)[1]
+        rozszerzenie = os.path.splitext(plik)[1].replace(".", "")
+        if rozszerzenie == "": rozszerzenie ="brak_rozszerzenia"
         nowa_nazwa = f"{nazwa}_{licznik}.{rozszerzenie}"
-        new_file_path = os.path.join(folder_path, nowa_nazwa)
+        if grupujPoExt:
+            folder_docelowy = os.path.join(folder_path, rozszerzenie)
+            os.makedirs(folder_docelowy, exist_ok=True)
+            new_file_path = os.path.join(folder_docelowy, nowa_nazwa)
+        else:
+            new_file_path = os.path.join(folder_path, nowa_nazwa)
 
         os.rename(file_path, new_file_path)
         licznik +=1
